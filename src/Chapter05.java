3a3928b09693ab52a587ddf20a5a287572b52d1f
akiikoisoe <180363344+akiso999@users.noreply.github.com>
2024-09-07 12:17:53 +0900

    新規Javaプロジェクト作成

---
+++ src/Chapter05.java
@@ -0,0 +1,20 @@
+
+public class Chapter05 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		String str1;  // String型の変数str1を宣言
+		String str2;  // String型の変数str1を宣言
+		int num1;  // String型の変数str1を宣言
+		
+		str1 = "Akihiko Isoe";  //str1を初期化
+		System.out.println(str1);
+
+		
+		str2 = "Sukina kotoba";  //str1を初期化
+		System.out.println(str2);
+		
+		num1 = 333;  //str1を初期化
+		System.out.println(num1);
+	}
+}
---
+++ src/Chapter06.java
@@ -0,0 +1,33 @@
+
+public class Chapter06 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+			long long1;
+			long1 = 10000000000L;
+			System.out.println(long1);
+			
+			double double1;
+			double1 = 3.14d;
+			System.out.println(double1);
+			
+			boolean boolean1;
+			boolean1 = true;
+			System.out.println(boolean1);
+
+			Integer integer1 = 100;  // 明示的なボクシング
+			System.out.println(integer1);
+			
+			Float float1 = 1.4142f;  // 明示的なボクシング
+			System.out.println(float1);
+			
+			Character character1 = 'う';  // 明示的なボクシング
+			System.out.println(character1);
+			
+			int[] array1 =  new int[] { 0, 10, 20, 30, 40 };
+			System.out.println(array1[3]);
+			System.out.println(array1.length);
+	}	
+	
+}
